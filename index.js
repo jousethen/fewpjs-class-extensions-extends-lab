8c1e21bc4b844788c3e1f5d69c99d41b11c93960
jousethen <jthen93@gmail.com>
2021-09-09 17:40:35 -0400

    tests pass

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length
+  }
+
+  get perimeter() {
+    return this.sides.reduce(function (acc, side) {
+      return acc + side;
+    });
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.countSides != 3) {
+      return false;
+    }
+    else if (this.sides[0] + this.sides[1] < this.sides[2] ||
+      this.sides[0] + this.sides[2] < this.sides[1] ||
+      this.sides[1] + this.sides[2] < this.sides[0]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (this.countSides != 4) {
+      return false;
+    } else if (this.perimeter % 4 != 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  get area() {
+    return this.sides[0] * this.sides[0];
+  }
+}
